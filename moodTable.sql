9bae0fbcdbbd7443d7eab197d0ff3225c006a526
sdk03 <sustainability1831@gmail.com>
2023-11-10 23:34:12 +0400

    i3

---
+++ moodTable.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2023 at 08:33 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mood`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mood`
+--
+
+CREATE TABLE `mood` (
+  `id` int(11) NOT NULL,
+  `person` text NOT NULL,
+  `mood` int(11) NOT NULL,
+  `remarks` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `password` varchar(256) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `mood`
+--
+
+INSERT INTO `mood` (`id`, `person`, `mood`, `remarks`, `timestamp`, `password`) VALUES
+(1, 'abu jaan', 1, 'too much traffic', '2023-11-10 13:50:25', ''),
+(2, 'ammi jaan', 3, 'maesha troubling', '2023-11-10 13:50:34', ''),
+(3, 'sadiq', 3, 'bad arguement', '2023-11-10 14:58:34', 'sadiq2003'),
+(4, 'adiba', 2, 'no laptop given', '2023-11-10 13:50:40', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mood`
+--
+ALTER TABLE `mood`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mood`
+--
+ALTER TABLE `mood`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
